c91239b10c22dab84f2d8058465d89387651787a
Šimon Nehéz <57033956+simon-ne@users.noreply.github.com>
2024-11-07 16:00:19 +0100

    relax the email validation in email_parameters

--- src/universal_watcher/notification_platforms/email/models/email_parameters.py
+++ src/universal_watcher/notification_platforms/email/models/email_parameters.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, field_validator
+import re
 
 from universal_watcher.core.classes.notification_platform.notification_platform_parameters import (
     NotificationPlatformParameters,
@@ -6,4 +7,14 @@ from universal_watcher.core.classes.notification_platform.notification_platform_
 
 
 class EmailParameters(BaseModel, NotificationPlatformParameters):
-    to: EmailStr = Field(..., description="Adresa príjemcu emailu.")
+    to: str = Field(..., description="Recipient email address.")
+
+    @field_validator("to")
+    def validate_email(cls, value):
+        if not value:
+            raise ValueError("Recipient email address must be provided.")
+
+        email_regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        if not re.match(email_regex, value):
+            raise ValueError("Invalid email address format.")
+        return value
